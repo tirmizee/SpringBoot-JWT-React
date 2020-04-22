b6f7269e347cbb57c638f00ff78d5fea3715b38d
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-04-23 02:56:33 +0700

    add constants

---
+++ coreui-react-admin/src/constants/index.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'http://localhost:8888';
+export const API_LOGIN_URI = API_BASE_URL + '/jwt/auth/token';
+export const API_LOGOUT_URI = API_BASE_URL + '/jwt/auth/token/revoke';
+
+export const ACCESS_TOKEN = 'accessToken';
