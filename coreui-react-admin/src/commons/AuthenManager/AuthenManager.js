8683ba19d1e1a8408dfc054d2195928ca722e621
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-04-23 02:56:08 +0700

    add constants

--- coreui-react-admin/src/commons/AuthenManager/AuthenManager.js
+++ coreui-react-admin/src/commons/AuthenManager/AuthenManager.js
@@ -1,24 +1,25 @@
 import APIManager from '../APIManager'
+import {ACCESS_TOKEN, API_LOGIN_URI, API_LOGOUT_URI} from '../../constants'
 
 class AuthenManager { 
 
   login(usernameAndPasswordBase64, callback) {
     const headers = { 'Authorization' : `Basic ${usernameAndPasswordBase64}` };
-    APIManager.methodPOST('http://localhost:8888/jwt/auth/token', {}, {headers}, callback);
+    APIManager.methodPOST(API_LOGIN_URI, {}, {headers}, callback);
   }
 
   logout(callback) {
-    const headers = { 'Authorization' : `Bearer ${localStorage.getItem('AccessToken')}` };
-    APIManager.methodGET('http://localhost:8888/jwt/auth/token/revoke', {headers}, callback);
+    const headers = { 'Authorization' : `Bearer ${localStorage.getItem(ACCESS_TOKEN)}` };
+    APIManager.methodGET(API_LOGOUT_URI, {headers}, callback);
   }
 
   setAuthenticated(token){
-    localStorage.setItem('AccessToken', token);
+    localStorage.setItem(ACCESS_TOKEN, token);
   }
 
   getCurrentUser(){
     try {
-      let token = localStorage.getItem('AccessToken');
+      let token = localStorage.getItem(ACCESS_TOKEN);
       return JSON.parse(atob(token.split('.')[1]));
     } catch (error) {
       console.log(error)
@@ -28,7 +29,7 @@ class AuthenManager {
 
   isAuthenticated() {
     let isAuthenticated = false;
-    let token = localStorage.getItem('AccessToken');
+    let token = localStorage.getItem(ACCESS_TOKEN);
     if(token == null) return isAuthenticated;
     let tokenBody = JSON.parse(atob(token.split('.')[1]));
     let date = new Date(tokenBody.exp);
